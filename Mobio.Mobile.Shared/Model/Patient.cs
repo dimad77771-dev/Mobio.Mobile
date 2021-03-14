5b0cd7f0f9ec0810917f7f327381db3e57a2abad
dima7777 <dima7777@DESKTOP-KCF43RE>
2021-03-15 00:19:06 +0200

    -

---
+++ Mobio.Mobile.Shared/Model/Patient.cs
@@ -0,0 +1,46 @@
+using OneBuilder.Mobile.Constants;
+using OneBuilder.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace OneBuilder.Model
+{
+	public class Patient : ViewModelBase
+	{
+		public Guid RowId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Gender { get; set; }
+		public string Phone { get; set; }
+		public string Email { get; set; }
+		public string IdentificationDocumentImage { get; set; }
+		public string IdentificationDocumentNumber { get; set; }
+		public DateTime? IdentificationDocumentExpDate { get; set; }
+		public string IdentificationDocumentType { get; set; }
+		public string Photo { get; set; }
+		public DateTime? BirthDate { get; set; }
+		public Guid? IdentificationDocumentRowId { get; set; }
+		public string AddressLine1 { get; set; }
+		public string AddressLine2 { get; set; }
+		public string City { get; set; }
+		public Guid? StateOrProvinceRowId { get; set; }
+		public int? CountryId { get; set; }
+		public string Postcode { get; set; }
+		public string HealthCareCardNumber { get; set; }
+		public string FamilyPhysicianName { get; set; }
+		public string FamilyPhysicianAddress { get; set; }
+		public string FamilyPhysicianPhone { get; set; }
+		public string FamilyPhysicianEmail { get; set; }
+		public string Ip_Created { get; set; }
+		public string Ip_Modified { get; set; }
+
+
+		public string FullPatientName => LastName + FirstName != "" ? LastName + ", " + FirstName : "New Record";
+		public Color TextColor { get; set; }
+		public Color BackgroundColor { get; set; }
+		public Color BorderColor { get; set; }
+	}
+}
--- Mobio.Mobile.Shared/ViewModels/RegisterViewModel.cs
+++ Mobio.Mobile.Shared/ViewModels/RegisterViewModel.cs
@@ -17,9 +17,12 @@ namespace OneBuilder.Mobile.ViewModels
 	public class RegisterViewModel : PageViewModel
 	{
 		public UserProfile Model { get; set; }
-		public State[] DdlStates { get; set; }
-		public string[] AAAA { get; set; } = new[] { "1", "2", "33" };
-		//url: "/utils/getCountries",
+		public State[] DdlStates { get; set; }  //url: "/utils/getCountries",
+		public ObservableCollection<Patient> Patients { get; set; }
+		public Patient SelectedPatient { get; set; }
+
+		public String PatientTabText { get; set; }
+
 
 		public Command CommitCommand => CommandFunc.CreateAsync(Commit);
 		public Command CancelCommand => CommandFunc.CreateAsync(Cancel);
@@ -70,6 +73,90 @@ namespace OneBuilder.Mobile.ViewModels
 
 				//Model.ProvinceOrState = DdlStates[1];
 				//Task.Delay
+
+				Patients = new[]
+				{
+					new Patient
+					{
+						RowId = Guid.NewGuid(),
+						FirstName = "Piter",
+						LastName = "Pen",
+					},
+					new Patient
+					{
+						RowId = Guid.NewGuid(),
+						FirstName = "Glen",
+						LastName = "Robinson",
+					},
+					new Patient
+					{
+						RowId = Guid.NewGuid(),
+						FirstName = "Gabija",
+						LastName = "Summers",
+					},
+					new Patient
+					{
+						RowId = Guid.NewGuid(),
+						FirstName = "Vincenzo",
+						LastName = "Saunders",
+					},
+					new Patient
+					{
+						RowId = Guid.NewGuid(),
+						FirstName = "Kalum",
+						LastName = "Edwards",
+					},
+					new Patient
+					{
+						RowId = Guid.NewGuid(),
+						FirstName = "Reggie",
+						LastName = "Neal",
+					},
+					new Patient
+					{
+						RowId = Guid.NewGuid(),
+						FirstName = "Torin",
+						LastName = "Mclean",
+					},
+					new Patient
+					{
+						RowId = Guid.NewGuid(),
+						FirstName = "Paulina",
+						LastName = "Valenzuela",
+					},
+				}.ToObservableCollection();
+
+				if (Patients.Any())
+				{
+					SelectedPatient = Patients.First();
+				}
+
+
+				RecalcPatients();
+
+				this.PropertyChanged += OnAnyPropertyChanged;
+			}
+		}
+
+		private void OnAnyPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == nameof(SelectedPatient))
+			{
+				RecalcPatients();
+			}
+		}
+
+		void RecalcPatients()
+		{
+			PatientTabText = "Patients (" + Patients.Count + ")";
+
+			foreach (var patient in Patients)
+			{
+				var selected = (patient == SelectedPatient);
+				patient.BackgroundColor = selected ? Color.FromHex("#d12323") : Color.Transparent;
+				patient.TextColor = selected ? Color.FromHex("#fff") : Color.FromHex("#333");
+				patient.BorderColor = selected ? Color.FromHex("#8c8c8c") : Color.FromHex("#8c8c8c");
+
 			}
 		}
 
